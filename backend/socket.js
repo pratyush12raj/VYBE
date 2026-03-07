32080b9eccbdcd8bfb924c40c3c42f2ac4274bde
Pratyush Raj <143428765+pratyush12raj@users.noreply.github.com>
2026-03-07 16:35:00 +0530

    Update socket.js

--- backend/socket.js
+++ backend/socket.js
@@ -45,7 +45,7 @@ const server = http.createServer(app)
 const io = new Server(server, {
   cors: {
     origin: [
-      "http://localhost:5173",
+      // "http://localhost:5173",
       "https://vybe-frontend-3spn.onrender.com"
     ],
     methods: ["GET", "POST"],
