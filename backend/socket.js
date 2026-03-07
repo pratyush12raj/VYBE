3a49246eb7ed90c109e0cf81766eb3df76c26b55
Pratyush Raj <143428765+pratyush12raj@users.noreply.github.com>
2026-03-07 16:29:40 +0530

    Update socket.js

--- backend/socket.js
+++ backend/socket.js
@@ -1,37 +1,80 @@
+// import http from "http"
+// import express from "express"
+// import { Server } from "socket.io"
+// const app=express()
+// const server=http.createServer(app)
+
+// const io=new Server(server,{
+//     cors:{
+//         origin:"http://localhost:5173",
+//         methods:["GET","POST"]
+//     }
+// })
+
+// const userSocketMap={}
+
+// export const getSocketId=(receiverId)=>{
+// return userSocketMap[receiverId]
+// }
+
+// io.on("connection",(socket)=>{
+//    const userId=socket.handshake.query.userId
+//    if(userId!=undefined){
+//     userSocketMap[userId]=socket.id
+//    }
+
+//  io.emit('getOnlineUsers',Object.keys(userSocketMap))  
+
+
+// socket.on('disconnect',()=>{
+//     delete userSocketMap[userId]
+//      io.emit('getOnlineUsers',Object.keys(userSocketMap))  
+// })
+
+// })
+
+
+
 import http from "http"
 import express from "express"
 import { Server } from "socket.io"
-const app=express()
-const server=http.createServer(app)
 
-const io=new Server(server,{
-    cors:{
-        origin:"http://localhost:5173",
-        methods:["GET","POST"]
-    }
+const app = express()
+const server = http.createServer(app)
+
+const io = new Server(server, {
+  cors: {
+    origin: [
+      "http://localhost:5173",
+      "https://vybe-frontend-3spn.onrender.com"
+    ],
+    methods: ["GET", "POST"],
+    credentials: true
+  }
 })
 
-const userSocketMap={}
+const userSocketMap = {}
 
-export const getSocketId=(receiverId)=>{
-return userSocketMap[receiverId]
+export const getSocketId = (receiverId) => {
+  return userSocketMap[receiverId]
 }
 
-io.on("connection",(socket)=>{
-   const userId=socket.handshake.query.userId
-   if(userId!=undefined){
-    userSocketMap[userId]=socket.id
-   }
+io.on("connection", (socket) => {
+  const userId = socket.handshake.query.userId
 
- io.emit('getOnlineUsers',Object.keys(userSocketMap))  
+  if (userId !== undefined) {
+    userSocketMap[userId] = socket.id
+  }
 
+  io.emit("getOnlineUsers", Object.keys(userSocketMap))
 
-socket.on('disconnect',()=>{
+  socket.on("disconnect", () => {
     delete userSocketMap[userId]
-     io.emit('getOnlineUsers',Object.keys(userSocketMap))  
+    io.emit("getOnlineUsers", Object.keys(userSocketMap))
+  })
 })
 
-})
+export { app, io, server }
 
 
 export {app,io, server}
